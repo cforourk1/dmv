b0688e06fae7b33d329530b16515a2092f58de7b
Crystal <cforourk@umich.edu>
2026-06-03 21:00:00 -0400

    added tables

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,20 @@ DROP TABLE IF EXISTS persons CASCADE;
 DROP TABLE IF EXISTS licenses;
 
 -- TODO: create "persons" and "licenses" tables
+
+
+CREATE TABLE persons (
+  id serial PRIMARY KEY,
+  first_name text NOT NULL,
+  last_name text NOT NULL
+);
+
+-- pk stands for Primary Key
+
+CREATE TABLE licenses (
+  id serial PRIMARY KEY,
+  address text NOT NULL,
+  eye_color text NOT NULL,
+  date_of_birth date NOT NULL,
+  person_id integer UNIQUE NOT NULL REFERENCES persons(id) ON DELETE CASCADE
+)
