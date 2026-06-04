f517ca7ae254000f4a61bbb5aaf50451917610aa
Crystal <cforourk@umich.edu>
2026-06-03 21:42:56 -0400

    completed

--- db/queries/licenses.js
+++ db/queries/licenses.js
@@ -12,7 +12,18 @@ export async function getLicenses() {
 
 /** @returns all licenses with their persons attached */
 export async function getLicensesIncludingPerson() {
-  // TODO
+  const sql = `
+  SELECT
+    *,
+    (
+      SELECT to_json(persons)
+      FROM persons
+      WHERE persons.id = licenses.person_id
+    ) AS person
+  FROM licenses
+  `;
+  const { rows: licenses } = await db.query(sql);
+  return licenses;
 }
 
 /** @returns the license specified by id */
@@ -30,7 +41,19 @@ export async function getLicenseById(id) {
 
 /** @returns the license specified by id with its person attached */
 export async function getLicenseByIdIncludingPerson(id) {
-  // TODO
+    const sql = `
+  SELECT
+    *,
+    (
+      SELECT to_json(persons)
+      FROM persons
+      WHERE persons.id = licenses.person_id
+    ) AS person
+  FROM licenses
+  WHERE licenses.id =$1
+  `;
+  const { rows: [license] } = await db.query(sql, [id]);
+  return license;
 }
 
 /**
@@ -38,5 +61,8 @@ export async function getLicenseByIdIncludingPerson(id) {
  * @returns undefined if the person does not have a license
  */
 export async function getLicenseByPersonId(id) {
-  // TODO
+  const sql = `
+  SELECT * FROM licenses WHERE person_id = $1`
+  const { rows: [license] } = await db.query(sql, [id]);
+  return license;
 }
